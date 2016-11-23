074f6f7a73183290a624e7ee69fe0ffc41b2e543
schroedingers.katze <schroedingers.katze@protonmail.com>
2016-11-24 00:27:50 +0100

    application of the DataReader class and logic to get data from the user

--- LibraryApp/src/pl/aleksandrawolny/App/Library.java
+++ LibraryApp/src/pl/aleksandrawolny/App/Library.java
@@ -1,13 +1,18 @@
 package pl.aleksandrawolny.App;
 
 import pl.aleksandrawolny.Data.*;
+import pl.aleksandrawolny.Utils.*;
 public class Library {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Book[] books = new Book[100];
+		DataReader dr = new DataReader();
 		books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 1999, 478, "Prószyński S-ka", "345676547");
+		dr.open();
+		books[1] = dr.readBook();
 		System.out.println(books[0].toString());
+		System.out.println(books[1].toString());
 		System.out.println("System może pomieścić " + books.length + " książek.");
+		dr.close();
 	}
 }
