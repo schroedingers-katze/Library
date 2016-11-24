18949d50d9e3ba266f950a4871cb61759b1d146f
schroedingers.katze <schroedingers.katze@protonmail.com>
2016-11-24 16:00:41 +0100

    new possible parent class Publication, Book will be able to extend it;

---
+++ LibraryApp/src/pl/aleksandrawolny/Data/Publication.java
@@ -0,0 +1,27 @@
+package pl.aleksandrawolny.Data;
+
+public class Publication {
+	private String title;
+	private String publisher;
+	private int year;
+	
+	//getters and setters
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
