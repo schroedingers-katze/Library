5494af400826bc5df0f582e81b06e73c9f143f89
schroedingers.katze <schroedingers.katze@protonmail.com>
2016-11-25 00:41:52 +0100

    added readBook() method

--- LibraryApp/src/pl/aleksandrawolny/Utils/DataReader.java
+++ LibraryApp/src/pl/aleksandrawolny/Utils/DataReader.java
@@ -43,4 +43,29 @@ public class DataReader {
 		Book book1 = new Book(title, author, year, pages, publisher, isbn);
 		return book1;
 	}
+	
+	public Magazine readMagazine(){
+		System.out.println("The title of the magazine: ");
+		String title = scan.nextLine();
+	
+		System.out.println("The day of the magazine: ");
+		int day = scan.nextInt();
+		scan.nextLine();
+		
+		System.out.println("The month of the magazine: ");
+		String month = scan.nextLine();
+
+		System.out.println("The year of the magazine: ");
+		int year = scan.nextInt();
+		scan.nextLine();
+		
+		System.out.println("The publisher of the book: ");
+		String publisher = scan.nextLine();
+
+		System.out.println("The language of the book: ");
+		String language = scan.nextLine();
+	
+		Magazine magazine = new Magazine(title, publisher, year, month, day, language);
+		return magazine;
+	}
 }
