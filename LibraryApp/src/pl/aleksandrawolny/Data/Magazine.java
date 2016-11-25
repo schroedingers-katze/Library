77dd7bf6a78bac7a7e4efa3d1093e2af288c8b57
schroedingers.katze <schroedingers.katze@protonmail.com>
2016-11-25 01:08:35 +0100

    generated hashCode() and equals() methods

--- LibraryApp/src/pl/aleksandrawolny/Data/Magazine.java
+++ LibraryApp/src/pl/aleksandrawolny/Data/Magazine.java
@@ -38,4 +38,38 @@ public class Magazine extends Publication{
 	public String toString() {
 		return "Magazine [title: " + getTitle() + ", publisher: " + getPublisher() + ", year:  " + getYear() + ", month: " + month + ", day: " + day + ", language: " + language + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + day;
+		result = prime * result + ((language == null) ? 0 : language.hashCode());
+		result = prime * result + ((month == null) ? 0 : month.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Magazine other = (Magazine) obj;
+		if (day != other.day)
+			return false;
+		if (language == null) {
+			if (other.language != null)
+				return false;
+		} else if (!language.equals(other.language))
+			return false;
+		if (month == null) {
+			if (other.month != null)
+				return false;
+		} else if (!month.equals(other.month))
+			return false;
+		return true;
+	}
+	
+	
 }
