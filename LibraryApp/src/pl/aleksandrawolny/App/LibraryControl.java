fc5755cc8a54ac4526fbe70fa016ff6f36909382
schroedingers.katze <schroedingers.katze@protonmail.com>
2016-11-28 00:18:37 +0100

    Option - LibraryControl inner class
    made the enum Option an inner class of the LibraryControl class;

--- LibraryApp/src/pl/aleksandrawolny/App/LibraryControl.java
+++ LibraryApp/src/pl/aleksandrawolny/App/LibraryControl.java
@@ -77,4 +77,32 @@ public class LibraryControl {
 	private void printMagazines(){
 		LibraryUtils.printMagazines(lb);
 	}
+	
+	public enum Option {
+		ADD_BOOK (1),
+		SEE_BOOKS (2),
+		ADD_MAGAZINE (3),
+		SEE_MAGAZINES (4),
+		EXIT (5);
+		
+		private int value;
+		
+		public int getValue() {
+			return value;
+		}
+
+		Option(int value){
+			this.value = value;
+		}
+		
+	    public static Option createFromInt(int option) throws NoSuchElementException{
+	    	Option result = null;
+	    	try{
+		        return Option.values()[option - 1];
+	    	} catch (ArrayIndexOutOfBoundsException e) {
+	    		throw new NoSuchElementException("Brak elementu o wskazanym ID");
+	    	}
+	    }
+	}
+	
 }
